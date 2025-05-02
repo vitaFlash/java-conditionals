4936621a95f8bbb4084c99abdb5b50ebdb1aec5a
vitaFlash <vitaliaflash@gmail.com>
2025-05-02 11:59:22 +0200

    weather

--- Weather.java
+++ Weather.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Weather {
     public static void main(String[] args) {
         //Escribe un programa que pida al usuario que ingrese la temperatura en celcius de su ciudad (La pregunta y la respuesta deberá estar en una sola línea) y según ello devuelva lo siguiente:
@@ -8,6 +10,22 @@ public class Weather {
         //Si es entre 26 y 35: Que calooorrrrr
         //Si es mayor a 36: Ahí no hay quien viva
 
-        //Imprime el resultado
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("¿Cuál es la temperatura en celcius de tu ciudad?");
+        int temperature = scanner.nextInt();
+        scanner.nextLine();
+        if (temperature < 0) {
+            System.out.println("Te estás congelando");
+        } else if (temperature >= 0 && temperature <= 10) {
+            System.out.println("Hace mucho frío");
+        } else if (temperature >= 11 && temperature <= 17) {
+            System.out.println("Con un abrigo estás bien");
+        } else if (temperature >= 18 && temperature <= 25) {
+            System.out.println("Parece que ha llegado el verano");
+        } else if (temperature >= 26 && temperature <= 35) {
+            System.out.println("Que calooorrrrr");
+        } else {
+            System.out.println("Ahí no hay quien viva");
+        }
     }
 }
